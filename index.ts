fb622fc63ee725b8cf84f5f28959195e7681bc39
Bilal-Gulzar <bilalgul415@gmail.com>
2024-03-26 15:39:31 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//Cli Guessing Number game.
+
+const num = Math.floor(Math.random() * 6 + 1);
+
+const guessingNumber = await inquirer.prompt([
+  {
+    message: "please guess a Number between 1-6:",
+    type: "number",
+    name: "userGuessingNumber",
+  },
+]);
+
+if (guessingNumber.userGuessingNumber === num) {
+  console.log("Congratulations! you guessed rigth Number.");
+} else {
+  console.log("Sorry, you guessed wrong Number.");
+}
+
